96bbbcf9916602741dbda82612dc542530909bff
vvduth <ducthai060501@gmail.com>
2025-12-10 21:15:43 +0200

    remove trash frontend

--- frontend/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // This URL is used for migrations and introspection
-    url: process.env.DATABASE_URL!,
-  },
-});
